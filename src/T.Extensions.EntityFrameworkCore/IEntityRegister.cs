2c43f76a8f35e115ea30b7fdbd18ae0c16b83a25
mabo <mabo7237@hotmail.com>
2022-07-06 18:17:58 +0800

    T.Extensions.EntityFrameworkCore 1.0.0 版本发布

---
+++ src/T.Extensions.EntityFrameworkCore/IEntityRegister.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace T.Extensions.EntityFrameworkCore
+{
+    /// <summary>
+    /// 数据库实体注册接口
+    /// </summary>
+    public interface IEntityRegister
+    {
+        /// <summary>
+        /// 将当前实体类映射对象注册到数据上下文模型构建器中
+        /// </summary>
+        /// <param name="modelBuilder">上下文模型构建器</param>
+        void RegistTo(ModelBuilder modelBuilder);
+    }
+}
---
+++ src/T.Extensions.EntityFrameworkCore/IEntityRegisterHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace T.Extensions.EntityFrameworkCore
+{
+    /// <summary>
+    /// 数据库实体注册接口辅助类
+    /// </summary>
+    public partial class IEntityRegisterHelper
+    {
+        /// <summary>
+        /// 返回数据库实体注册接口实例集合
+        /// </summary>
+        /// <param name="path">文件目录</param>
+        /// <param name="searchPattern">包含通配符(*和?)的程序集文件搜索字符串</param>
+        /// <returns></returns>
+        public static IEnumerable<IEntityRegister> FindRegisters(string path, string searchPattern = "*.dll")
+        {
+            var files = Directory.GetFiles(path, searchPattern);
+            foreach (var file in files)
+            {
+                Assembly assembly = Assembly.LoadFrom(file);
+                foreach (var x in assembly.DefinedTypes)
+                {
+                    if (x.IsClass && !x.IsAbstract && typeof(IEntityRegister).IsAssignableFrom(x))
+                    {
+                        IEntityRegister entityRegister = Activator.CreateInstance(x) as IEntityRegister;
+                        yield return entityRegister;
+                    }
+                }
+            }
+        }
+    }
+
+}
